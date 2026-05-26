969739b86911c01434a0047517a9d67099a0b084
faisalaiagent <faisalagentai@gmail.com>
2026-05-26 16:14:46 +0500

    app

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+    formats: ['image/webp', 'image/avif'],
+  },
+}
+
+export default nextConfig
